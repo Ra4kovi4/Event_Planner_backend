5f96b8fa9c9ce192e951822e979a8b5302f49775
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-08-31 15:06:06 +0300

    add eventsCount

--- services/plannerService/getAllEvents.js
+++ services/plannerService/getAllEvents.js
@@ -2,14 +2,15 @@ const { Events } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const getAllEvents = async (data) => {
-	const { page, limit } = data;
+	const { page = 1, limit = 8 } = data;
 	const skip = (page - 1) * limit;
 	const events = await Events.find({}, "-createdAt -updatedAt", {
 		skip,
 		limit,
 	}).sort({ createdAt: -1 });
+	const eventsCount = await Events.find({}).countDocuments();
 
-	return events;
+	return { events, eventsCount };
 };
 
 module.exports = { getAllEvents };
